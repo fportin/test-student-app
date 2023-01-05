a824340377faff7dbd67e795b543e862386402e7
Franco Portin <fportin350@gmail.com>
2023-01-04 17:46:33 -0800

    stock sybol length

--- backend/app/seeds/stocks.py
+++ backend/app/seeds/stocks.py
@@ -7,8 +7,9 @@ def seed_stocks():
         for row in csv_file.readlines()[1:]:
             stock_symbol, company_name = row.split(',')[:2]
             company = company_name.strip()
+            stock = Stock(stock_symbol=stock_symbol, company_name=company)
             # print("SEEDS____________________________________________", stock_symbol)
-            db.session.add(Stock(stock_symbol=stock_symbol, company_name=company))
+            db.session.add(stock)
 
     db.session.commit()
 # def seed_stocks():
